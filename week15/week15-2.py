9291758b9c270f18f3d07559c7f79c0bdafbd3e3
SQRBEAN <ryanwei.tw521@gmail.com>
2023-12-22 11:47:58 +0800

    week15

---
+++ week15/week15-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        N=len(s)
+        zero = 0
+        one=0
+        for i in range(N):
+            if s[i]=='1':one +=1
+    #print('zero',zero,'one',one)
+        ans=0
+        for i in range(N-1):
+            if s[i]=='0':
+                zero+=1
+            if s[i]=='1':
+                one-=1
+            ans=max(ans,zero+one)
+        return ans
